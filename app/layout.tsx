f0ed3f0db9392c8262220aafad906d07a238631f
Lars Van Der Grift <lavan-de@f1r1s17.codam.nl>
2026-02-05 16:09:22 +0100

    Google Analytics, test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { siteConfig } from "@/config/site";
 
@@ -47,6 +48,20 @@ export default function RootLayout({
       <body
         className={`${inter.variable} font-sans antialiased bg-slate-900 text-white`}
       >
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WNL18YFW1X"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WNL18YFW1X');
+          `}
+        </Script>
+        
         {children}
       </body>
     </html>
